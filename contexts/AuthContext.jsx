8e5497d3f3b0b626578ec59b809a6ea8af435d62
rodrigobergamindev <rodrigobergamindev@gmail.com>
2021-12-14 21:48:26 -0300

    .

--- contexts/AuthContext.jsx
+++ contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import {setCookie, parseCookies} from 'nookies'
 import { api } from '../services/api'
-
+import Router from 'next/router'
 
 
 
@@ -8,9 +9,25 @@ export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
     
+
     const [user, setUser] = useState()
     
-    const isAuthenticated = false;
+    const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const {'nextauth.token': token} = parseCookies()
+
+        if(token) {
+            api.get('/me').then(response => {
+                const {email, permissions, roles} = response.data
+                setUser({
+                    email,
+                    permissions,
+                    roles
+                })
+            })
+        }
+    }, [])
 
     async function signIn({email, password}) {
        try {
@@ -19,7 +36,29 @@ export function AuthProvider({ children }) {
             password
         })
 
-        console.log(response.data)
+        const {token, refreshToken, permissions, roles} = response.data
+
+        setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+        })
+        setCookie(undefined, 'nextauth.refreshToken', refreshToken,  {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+        })
+
+        //localStorage --> apenas do lado cliente
+        //sessionStorage --> Funcionamento enquanto o usuário está com uma sessão ativa
+        //cookies --> lado cliente e lado servidor
+
+        setUser({
+            email,
+            permissions,
+            roles
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+        Router.push('/dashboard')
 
        } catch (error) {
            console.log(error)
@@ -27,7 +66,7 @@ export function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider value={{signIn, isAuthenticated}}>
+        <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
             {children}
         </AuthContext.Provider>
     )
